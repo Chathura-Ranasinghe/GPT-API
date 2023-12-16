65f51f22c64197b7325bd41af89ceede7518d1db
Chathura Ranasinghe <chathurar.99.bc@gmail.com>
2023-12-17 01:28:58 +0530

    chat_log

--- LANGCHAIN/langchain-test_Chat.py
+++ LANGCHAIN/langchain-test_Chat.py
@@ -1,18 +1,49 @@
 import os
 import sys
+from datetime import datetime
 
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.chat_models import ChatOpenAI
 
+# Redirect stderr to null
+sys.stderr = open(os.devnull, 'w')
+
 loader = TextLoader('data.txt')
 index = VectorstoreIndexCreator().from_loaders([loader])
 
-# Check if there is at least one command-line argument
-if len(sys.argv) > 1:
-    query = sys.argv[1]
-    print(index.query(query, llm=ChatOpenAI()))
-else:
-    print("No command-line arguments provided.")
+log_file = open("chat_log.txt", "a")
+
+print("\ngpt  > Welcome to the chat! Type 'goodbye' to exit.")
+
+conversation_history = []
+
+while True:
+    # Prompt the user for input
+    query = input("\nuser > ")
+
+    if query:
+        # Append the user's query to the conversation history
+        conversation_history.append(f"{query}")
+
+        # Pass the entire conversation history to the model
+        result = index.query("\n".join(conversation_history), llm=ChatOpenAI())
+
+        try:
+            result = index.query("\n".join(conversation_history), llm=ChatOpenAI())
+            print("gpt  > ",result)
+            conversation_history.append(f"{result}")
+        except Exception as e:
+            print("gpt  > An error occurred:", str(e))
 
 
+        # Append the model's response to the conversation history
+        conversation_history.append(f"{result}")
+
+        log_file.write(f"{datetime.now()} - User: {query}, GPT: {result}\n")
+        
+    else:
+        print("gpt  > No query provided.")
+
+    if query.lower() == "goodbye":
+        break
